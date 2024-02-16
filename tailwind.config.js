b6687cdc1dc1c89f4bd7814f5687e20462fccedb
BernardoFOFG <bernardofofg@gmail.com>
2024-02-16 01:39:51 -0300

    add: font spartans

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    fontFamily: {
+      sans: ["League Spartas", "sans-serif"],
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
