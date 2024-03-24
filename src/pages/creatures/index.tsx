40ff4569c962efd027f1b8994850043c2599d730
BernardoFOFG <bernardofofg@gmail.com>
2024-03-24 16:16:13 -0300

    add: creatures page

--- src/pages/creatures/index.tsx
+++ src/pages/creatures/index.tsx
@@ -1,6 +1,67 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ClassesDialog } from "../classes/dialog";
+import { Pagination } from "@/components/Pagination";
+
+export interface CreaturesProps {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  drops: string[];
+}
+
 export const Creature = () => {
-    return (
-      <div>Creature</div>
-    )
-  }
-  
+  const [data, setData] = useState<CreaturesProps[]>([]);
+  const [count, setCount] = useState(16);
+  const [page, setPage] = useState(0);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://eldenring.fanapis.com/api/creatures?limit=20&page=${page}`
+      );
+      setData(response.data.data);
+      setCount(response.data.count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+  return (
+    <div>
+      Creature
+      <div className="flex overflow-y-auto max-h-[calc(100vh-8rem)] min-h-[calc(100vh-8rem)] p-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
+          {data.map((value) =>
+            !value.image ? (
+              <></>
+            ) : (
+              <ClassesDialog data={value.id}>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <img
+                    src={value.image}
+                    className="size-32 object-contain"
+                    title={value.name}
+                  />
+                  <p className="font-semibold">{value.name}</p>
+                </div>
+              </ClassesDialog>
+            )
+          )}
+        </div>
+      </div>
+      {count > 16 && (
+        <Pagination
+          itemsPerPage={16}
+          page={page}
+          setPage={setPage}
+          totalCount={count}
+        />
+      )}
+    </div>
+  );
+};
