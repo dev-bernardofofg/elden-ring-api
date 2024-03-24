024727433f24d95ae592ef39aebcb35e75b4f516
BernardoFOFG <bernardofofg@gmail.com>
2024-03-24 18:20:39 -0300

    add: shield page

---
+++ src/pages/shield/dialog.tsx
@@ -0,0 +1,138 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ReactNode, useEffect, useState } from "react";
+import { ShieldProps } from ".";
+import axios from "axios";
+import { Dialog } from "@/components/Dialog";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useToast } from "@/components/ui/use-toast";
+
+interface ShieldDialogProps {
+  children: ReactNode;
+  data: any;
+}
+
+export const ShieldDialog = ({ children, data }: ShieldDialogProps) => {
+  const [datas, setData] = useState<ShieldProps | null>(null);
+  const { toast } = useToast();
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://eldenring.fanapis.com/api/shields/${data}`
+      );
+      setData(response.data.data);
+    } catch {
+      toast({
+        title: "Erro!",
+        description: "Houve um problema na sua requisição. ",
+        className: "bg-red-500 text-gray-200 border-0",
+      });
+    }
+  };
+
+  console.log(datas);
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Dialog
+      content={
+        <>
+          <DialogContent className="flex flex-row-reverse bg-yellow-800 text-stone-200 p-0 border-4 border-stone-800">
+            <DialogHeader className="bg-stone-700 rounded-t h-full flex items-center">
+              <img
+                src={datas?.image}
+                className="h-[28rem] max-w-full min-w-64 w-full object-contain"
+              />
+              <div className="p-4 !my-0">
+                <DialogTitle>{datas?.name}</DialogTitle>
+              </div>
+            </DialogHeader>
+
+            <DialogDescription className="text-stone-300 gap-4 flex flex-col p-4">
+              <div>
+                <p className="font-bold text-lg">{datas?.name}</p>
+                <p className="font-bold text-xs">{datas?.description}</p>
+              </div>
+
+              <div className="flex flex-col gap-4">
+                <p>
+                  <span className="font-bold">Category: </span>
+                  {datas?.category}
+                </p>
+                <div className="grid grid-cols-2 flex-col gap-2">
+                  <div>
+                    <p className="font-bold text-stone-200">Scale With</p>
+                    {datas?.scalesWith.map((value) => (
+                      <div>
+                        <span
+                          className={`capitalize font-bold text-stone-300 mr-1 text-xs`}
+                        >
+                          {value.name}:
+                        </span>
+                        <span>{value.scaling}</span>
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    <p className="font-bold text-stone-200">Req Attributes</p>
+                    {datas?.requiredAttributes.map((value) => (
+                      <div>
+                        <span
+                          className={`capitalize font-bold text-stone-300 mr-1 text-xs`}
+                        >
+                          {value.name}:
+                        </span>
+                        <span>{value.amount}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid grid-cols-2 flex-col gap-2">
+                <div>
+                  <p className="font-bold text-stone-200">Attack</p>
+                  {datas?.attack.map((value) => (
+                    <div>
+                      <span
+                        className={`capitalize font-bold text-stone-300 mr-1 text-xs`}
+                      >
+                        {value.name}:
+                      </span>
+                      <span>{value.amount}</span>
+                    </div>
+                  ))}
+                </div>
+
+                <div>
+                  <p className="font-bold text-stone-200">Defense</p>
+
+                  {datas?.defence.map((value) => (
+                    <div>
+                      <span
+                        className={`capitalize font-bold text-stone-300 mr-1 text-xs`}
+                      >
+                        {value.name}:
+                      </span>
+                      <span>{value.amount}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogContent>
+        </>
+      }
+      title={datas?.name}
+    >
+      {children}
+    </Dialog>
+  );
+};
--- src/pages/shield/index.tsx
+++ src/pages/shield/index.tsx
@@ -1,6 +1,92 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ShieldDialog } from "./dialog";
+import { Pagination } from "@/components/Pagination";
+
+export interface ShieldProps {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  category: string;
+  weight: number;
+  attack: AttackProps[];
+  defence: DefenceProps[];
+  requiredAttributes: RequiredAttributesProps[];
+  scalesWith: ScalesWithProps[];
+}
+
+interface AttackProps {
+  name: string;
+  amount: number;
+}
+
+interface DefenceProps {
+  name: string;
+  amount: number;
+}
+
+interface RequiredAttributesProps {
+  name: string;
+  amount: number;
+}
+
+interface ScalesWithProps {
+  name: string;
+  scaling: string;
+}
+
 export const Shield = () => {
-    return (
-      <div>Shield</div>
-    )
-  }
-  
+  const [data, setData] = useState<ShieldProps[]>([]);
+  const [count, setCount] = useState(16);
+  const [page, setPage] = useState(0);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://eldenring.fanapis.com/api/shields?limit=16&page=${page}`
+      );
+
+      setData(response.data.data);
+      setCount(response.data.count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  return (
+    <div>
+      Shield
+      <div className="flex overflow-y-auto max-h-[calc(100vh-8rem)] min-h-[calc(100vh-8rem)] p-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
+          {data.map((value) =>
+            !value.image ? (
+              <></>
+            ) : (
+              <ShieldDialog data={value.id} key={value.id}>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <img
+                    src={value.image}
+                    className="size-32 object-cover"
+                    title={value.name}
+                  />
+                  <p className="font-semibold">{value.name}</p>
+                </div>
+              </ShieldDialog>
+            )
+          )}
+        </div>
+      </div>
+      <Pagination
+        itemsPerPage={16}
+        page={page}
+        setPage={setPage}
+        totalCount={count}
+      />
+    </div>
+  );
+};
