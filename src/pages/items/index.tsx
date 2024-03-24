ca6740e6c543b56531eb299c7ecfb6bc53f66301
BernardoFOFG <bernardofofg@gmail.com>
2024-03-24 17:33:58 -0300

    add: item page

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,6 +1,67 @@
+import { Pagination } from "@/components/Pagination";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ItemDialog } from "./dialog";
+
+export interface ItemProps {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  type: string;
+  effect: string;
+}
+
 export const Items = () => {
-    return (
-      <div>Items</div>
-    )
-  }
-  
+  const [data, setData] = useState<ItemProps[]>([]);
+  const [count, setCount] = useState(16);
+  const [page, setPage] = useState(0);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `	https://eldenring.fanapis.com/api/items?limit=16&page=${page}`
+      );
+
+      setData(response.data.data);
+      setCount(response.data.count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+  return (
+    <div>
+      Items
+      <div className="flex overflow-y-auto max-h-[calc(100vh-8rem)] min-h-[calc(100vh-8rem)] p-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
+          {data.map((value) =>
+            !value.image ? (
+              <></>
+            ) : (
+              <ItemDialog data={value.id}>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <img
+                    src={value.image}
+                    className="size-32"
+                    title={value.name}
+                  />
+                  <p className="font-semibold">{value.name}</p>
+                </div>
+              </ItemDialog>
+            )
+          )}
+        </div>
+      </div>
+      <Pagination
+        itemsPerPage={16}
+        page={page}
+        setPage={setPage}
+        totalCount={count}
+      />
+    </div>
+  );
+};
