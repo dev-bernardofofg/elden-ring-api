bbed5a7d42cc36d8d1c3d289f3873c1143e9ac81
BernardoFOFG <bernardofofg@gmail.com>
2024-03-24 04:42:19 -0300

    add: classes pages

--- src/pages/classes/index.tsx
+++ src/pages/classes/index.tsx
@@ -1,6 +1,77 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ClassesDialog } from "./dialog";
+import { Pagination } from "@/components/Pagination";
+
+export interface ClassesProps {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  stats: {
+    level: string;
+    vigor: string;
+    mind: string;
+    endurance: string;
+    strength: string;
+    dexterity: string;
+    intelligence: string;
+    faith: string;
+    arcane: string;
+  };
+}
+
 export const Classes = () => {
-    return (
-      <div>Classes</div>
-    )
-  }
-  
+  const [data, setData] = useState<ClassesProps[]>([]);
+  const [count, setCount] = useState(16);
+  const [page, setPage] = useState(0);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://eldenring.fanapis.com/api/classes?limit=16&page=${page}`
+      );
+      setData(response.data.data);
+      setCount(response.data.count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+  return (
+    <div>
+      Classes
+      <div className="flex overflow-y-auto max-h-[calc(100vh-8rem)] min-h-[calc(100vh-8rem)] p-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
+          {data.map((value) =>
+            !value.image ? (
+              <></>
+            ) : (
+              <ClassesDialog data={value.id}>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <img
+                    src={value.image}
+                    className="size-32 object-contain"
+                    title={value.name}
+                  />
+                  <p className="font-semibold">{value.name}</p>
+                </div>
+              </ClassesDialog>
+            )
+          )}
+        </div>
+      </div>
+      {count > 16 && (
+        <Pagination
+          itemsPerPage={16}
+          page={page}
+          setPage={setPage}
+          totalCount={count}
+        />
+      )}
+    </div>
+  );
+};
