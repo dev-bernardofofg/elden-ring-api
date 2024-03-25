d4bdade944c333c48712efca271021d3d99b2f56
BernardoFOFG <bernardofofg@gmail.com>
2024-03-25 16:14:16 -0300

    add: talismans page

---
+++ src/pages/talismans/dialog.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ReactNode, useEffect, useState } from "react";
+import { TalismanProps } from ".";
+import axios from "axios";
+import { Dialog } from "@/components/Dialog";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useToast } from "@/components/ui/use-toast";
+
+interface TalismanDialogProps {
+  children: ReactNode;
+  data: any;
+}
+
+export const TalismanDialog = ({ children, data }: TalismanDialogProps) => {
+  const [datas, setData] = useState<TalismanProps | null>(null);
+  const { toast } = useToast();
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://eldenring.fanapis.com/api/talismans/${data}`
+      );
+      setData(response.data.data);
+    } catch {
+      toast({
+        title: "Erro!",
+        description: "Houve um problema na sua requisição. ",
+        className: "bg-red-500 text-gray-200 border-0",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Dialog
+      content={
+        <>
+          <DialogContent className="flex flex-row-reverse bg-yellow-800 text-stone-200 p-0 border-4 border-stone-800">
+            <DialogHeader className="bg-stone-700 rounded-t h-full flex items-center">
+              <img
+                src={datas?.image}
+                className="h-[28rem] max-w-full min-w-64 w-full object-contain"
+              />
+              <div className="p-4 !my-0">
+                <DialogTitle>{datas?.name}</DialogTitle>
+              </div>
+            </DialogHeader>
+
+            <DialogDescription className="text-stone-300 gap-4 flex flex-col p-4">
+              <div>
+                <p className="font-bold text-lg">{datas?.name}</p>
+                <p className="font-bold">{datas?.description}</p>
+              </div>
+
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <p>
+                    <span className="font-bold">Effects: </span>
+                    {datas?.effect}
+                  </p>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogContent>
+        </>
+      }
+      title={datas?.name}
+    >
+      {children}
+    </Dialog>
+  );
+};
--- src/pages/talismans/index.tsx
+++ src/pages/talismans/index.tsx
@@ -1,6 +1,65 @@
+import { Pagination } from "@/components/Pagination";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { TalismanDialog } from "./dialog";
+
+export interface TalismanProps {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  effect: string;
+}
 export const Talismans = () => {
-    return (
-      <div>Talismans</div>
-    )
-  }
-  
+  const [data, setData] = useState<TalismanProps[]>([]);
+  const [count, setCount] = useState(16);
+  const [page, setPage] = useState(0);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://eldenring.fanapis.com/api/talismans?limit=16&page=${page}`
+      );
+
+      setData(response.data.data);
+      setCount(response.data.count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+  return (
+    <div>
+      Talismans
+      <div className="flex overflow-y-auto max-h-[calc(100vh-8rem)] min-h-[calc(100vh-8rem)] p-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
+          {data.map((value) =>
+            !value.image ? (
+              <></>
+            ) : (
+              <TalismanDialog data={value.id} key={value.id}>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <img
+                    src={value.image}
+                    className="size-32 object-cover"
+                    title={value.name}
+                  />
+                  <p className="font-semibold">{value.name}</p>
+                </div>
+              </TalismanDialog>
+            )
+          )}
+        </div>
+      </div>
+      <Pagination
+        itemsPerPage={16}
+        page={page}
+        setPage={setPage}
+        totalCount={count}
+      />
+    </div>
+  );
+};
